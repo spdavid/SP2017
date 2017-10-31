3e789eb3542edd914c65dbeade382f882cb35ccd
David Opdendries <david@zalo.se>
2017-10-31 11:03:19 +0100

    MVC Intro

---
+++ RefreshMVC/RefreshMVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RefreshMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            SetViewBag();
+
+            List<string> cars = new List<string>();
+            cars.Add("Volvo");
+            cars.Add("Audi");
+            cars.Add("Mazda");
+            cars.Add("KIA");
+
+            ViewBag.carsarray = cars;
+
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            SetViewBag();
+            return View();
+        }
+
+        public void SetViewBag()
+        {
+            ViewBag.foo = "bar";
+            ViewBag.MyName = "David";
+        }
+    }
+}
