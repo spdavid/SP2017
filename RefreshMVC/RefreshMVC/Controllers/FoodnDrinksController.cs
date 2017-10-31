b846feddd0eb796981f218a193a9450030848aa5
David Opdendries <david@zalo.se>
2017-10-31 13:47:03 +0100

    Latest

---
+++ RefreshMVC/RefreshMVC/Controllers/FoodnDrinksController.cs
@@ -0,0 +1,45 @@
+﻿using RefreshMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RefreshMVC.Controllers
+{
+    public class FoodnDrinksController : Controller
+    {
+        // GET: FoodnDrinks
+        public ActionResult Food()
+        {
+            List<string> foods = new List<string>();
+            foods.Add("Pizza");
+            foods.Add("Fries");
+            foods.Add("ShellFish");
+
+            ViewBag.FoodItems = foods;
+
+            return View();
+        }
+
+        public ActionResult Drinks()
+        {
+            List<Drink> drinks = new List<Drink>
+            {
+                new Drink() { Title = "Soda", Url = "http://media.beam.usnews.com/28/76/090af7a34b4681895921789ffe6b/150617-sodaglass-stock.jpg" },
+                new Drink() { Title = "Milk", Url = "https://upload.wikimedia.org/wikipedia/commons/0/0e/Milk_glass.jpg" },
+                new Drink() { Title = "Beer", Url = "https://www.drinkpreneur.com/wp-content/uploads/2017/04/drinkpreneur_2016-01-26-1453821995-8643361-beermain.jpg" },
+                new Drink() { Title = "Vodka", Url = "https://www.wikihow.com/images/thumb/f/f4/Drink-Vodka-Step-3-preview.jpg/550px-nowatermark-Drink-Vodka-Step-3-preview.jpg" }
+            };
+
+            ViewBag.Drinks = drinks;
+
+            return View();
+        }
+
+        public ActionResult AnotherPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ RefreshMVC/RefreshMVC/Models/Drink.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RefreshMVC.Models
+{
+    public class Drink
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+
+    }
+}
