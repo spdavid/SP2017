98ce48d4e208dcbcaab57e1021b664c630aad720
David Opdendries <david@zalo.se>
2017-11-08 13:49:04 +0100

    DB Entity framework

---
+++ Developer2/DBFirstEntityFramework/DBFirstEntityFramework/Controllers/SchoolsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DBFirstEntityFramework.Models;
+
+namespace DBFirstEntityFramework.Controllers
+{
+    public class SchoolsController : Controller
+    {
+        private SchoolDatabaseEntities db = new SchoolDatabaseEntities();
+
+        // GET: Schools
+        public ActionResult Index()
+        {
+            return View(db.Schools.ToList());
+        }
+
+        // GET: Schools/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            School school = db.Schools.Find(id);
+            if (school == null)
+            {
+                return HttpNotFound();
+            }
+            return View(school);
+        }
+
+        // GET: Schools/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schools/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] School school)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Schools.Add(school);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(school);
+        }
+
+        // GET: Schools/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            School school = db.Schools.Find(id);
+            if (school == null)
+            {
+                return HttpNotFound();
+            }
+            return View(school);
+        }
+
+        // POST: Schools/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] School school)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(school).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(school);
+        }
+
+        // GET: Schools/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            School school = db.Schools.Find(id);
+            if (school == null)
+            {
+                return HttpNotFound();
+            }
+            return View(school);
+        }
+
+        // POST: Schools/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            School school = db.Schools.Find(id);
+            db.Schools.Remove(school);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
