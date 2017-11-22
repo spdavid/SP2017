811ffb5ed59a3ca0676884a9e4b6fc818c985074
David Opdendries <david@zalo.se>
2017-11-22 12:04:06 +0100

    javascript example

---
+++ Developer2/JavascriptIntro/JavascriptIntro/Scripts/Calculate.js
@@ -0,0 +1,39 @@
+﻿
+if ("5" == 5)
+{
+    console.log("they are the same");
+}
+
+if ("5" === 5) {
+    console.log("they are the same 2");
+}
+else
+{
+    console.log("they are not the same 2");
+}
+
+
+function calc(num1, num2)
+{
+    var newNum = num1 + num2;
+
+   var mainElement = document.getElementById('main');
+   mainElement.innerHTML = "<div>" + newNum + "</div>";
+}
+
+function calcInputs() {
+    var num1 = parseInt(document.getElementById('firstNumber').value);
+    var num2 = parseInt(document.getElementById('secondNumber').value);
+
+    var newNum = num1 + num2;
+
+    var mainElement = document.getElementById('main');
+    mainElement.innerHTML = "<div>" + newNum + "</div>";
+    
+}
+
+
+//window.onload = function ()
+//{
+//    calc(5, 3);
+//}
---
+++ Developer2/JavascriptIntro/JavascriptIntro/Scripts/Main.js
@@ -0,0 +1,24 @@
+﻿
+
+function myFunction(additionaltext) {
+    var myFirstVar = "hello world";
+    var myint = 3;
+    console.log(myFirstVar + additionaltext);
+
+    var person = {
+        name: "david",
+        age : 37
+    };
+
+    person.height = 182;
+
+    console.log(person);
+
+    var mainDivElement = document.getElementById('main');
+    mainDivElement.innerText = "Hello my name is " + person.name;
+
+}
+
+window.onload = function () {
+    myFunction(" hello folkis");
+}
