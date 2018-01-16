8b6c0b7d4297ebdc82abd614a53506dc2c2d79d7
David Opdendries <david@zalo.se>
2018-01-16 11:13:21 +0100

    CSOM 101

---
+++ OfficeDev1/MyFirstConsoleSPApplication/MyFirstConsoleSPApplication/CodeExamples/CSOM101.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleSPApplication.CodeExamples
+{
+   public class CSOM101
+    {
+        public static void GetWebTitle(ClientContext context)
+        {
+            //Web w1 = context.Web;
+
+            //// take your order
+            //context.Load(w1);
+            //// Goes and gets it. Takes about 400 ms or longer
+            //context.ExecuteQuery();
+            //Console.WriteLine(w1.Title);
+            //Console.WriteLine(w1.Url);
+
+            // the below code is faster as we are only requesting 
+            // the properties we want from the web object
+            Web w2 = context.Web;
+            // only get the propeties you need
+            context.Load(w2, w => w.Title, w => w.Url);
+            // Goes and gets it. 
+            context.ExecuteQuery();
+            Console.WriteLine(w2.Title);
+            Console.WriteLine(w2.Url);
+
+        }
+
+
+    }
+}
---
+++ OfficeDev1/MyFirstConsoleSPApplication/MyFirstConsoleSPApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using System.Security;
+
+namespace MyFirstConsoleSPApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ClientContext ctx = Helpers.ContextHelper.GetClientContext("https://folkis2017.sharepoint.com/sites/David"))
+            {
+                CodeExamples.CSOM101.GetWebTitle(ctx);
+            }
+
+                Console.WriteLine("press enter to continue");
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
