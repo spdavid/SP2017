283b27855c405ee82215e1022b5c87590262bf91
David Opdendries <david@zalo.se>
2018-01-16 12:05:32 +0100

    lists

--- OfficeDev1/MyFirstConsoleSPApplication/MyFirstConsoleSPApplication/CodeExamples/CSOM101.cs
+++ OfficeDev1/MyFirstConsoleSPApplication/MyFirstConsoleSPApplication/CodeExamples/CSOM101.cs
@@ -24,12 +24,81 @@ namespace MyFirstConsoleSPApplication.CodeExamples
             // the properties we want from the web object
             Web w2 = context.Web;
             // only get the propeties you need
-            context.Load(w2, w => w.Title, w => w.Url);
+            context.Load(w2, 
+                w => w.Title, 
+                w => w.Url);
             // Goes and gets it. 
             context.ExecuteQuery();
             Console.WriteLine(w2.Title);
             Console.WriteLine(w2.Url);
 
+        }
+
+        public static void UpdateTitleOfWeb(ClientContext ctx, string NewTitle)
+        {
+            Web web = ctx.Web;
+
+            web.Title = NewTitle;
+            web.Update();
+
+            ctx.ExecuteQuery();
+
+        }
+
+        public static void ListAllLists (ClientContext ctx)
+        {
+            ListCollection lists = ctx.Web.Lists;
+            // ctx.Load(lists); Takes longer as it gets everything
+            ctx.Load(lists, lsts => lsts.Include(
+                l => l.Title,
+                l => l.DefaultViewUrl));
+            ctx.ExecuteQuery();
+
+
+            foreach (var list in lists)
+            {
+                Console.WriteLine(list.Title);
+                Console.WriteLine(list.DefaultViewUrl);
+
+            }
+
+        }
+
+        public static void CreateDocumentLibrary(ClientContext ctx)
+        {
+            ListCreationInformation info = new ListCreationInformation();
+            info.Title = "New Doc Library";
+            info.TemplateType = 101;
+            info.Description = "davids cool library";
+            info.Url = "NewDocLib";
+            ctx.Web.Lists.Add(info);
+
+            ctx.ExecuteQuery();
+        }
+
+        public static void CreateGenericList(ClientContext ctx)
+        {
+            var lists = ctx.Web.Lists;
+            var results = ctx.LoadQuery(lists.Where(list => list.Title == "Custom List"));
+            ctx.Web.Context.ExecuteQuery();
+
+            // same as below
+           // if (results.Count() > 0)
+            if (!results.Any())
+            {
+                ListCreationInformation info = new ListCreationInformation();
+                info.Title = "Custom List";
+                info.TemplateType = 100;
+                info.Description = "Custom List";
+                info.Url = "lists/customlist";
+                ctx.Web.Lists.Add(info);
+
+                ctx.ExecuteQuery();
+            }
+            else
+            {
+                Console.WriteLine("List already exists. Give it another url and name");
+            }
         }
 
 
--- OfficeDev1/MyFirstConsoleSPApplication/MyFirstConsoleSPApplication/Program.cs
+++ OfficeDev1/MyFirstConsoleSPApplication/MyFirstConsoleSPApplication/Program.cs
@@ -12,12 +12,20 @@ namespace MyFirstConsoleSPApplication
     {
         static void Main(string[] args)
         {
+            // using statement will dispose your object when its done
             using (ClientContext ctx = Helpers.ContextHelper.GetClientContext("https://folkis2017.sharepoint.com/sites/David"))
             {
-                CodeExamples.CSOM101.GetWebTitle(ctx);
+                // CodeExamples.CSOM101.GetWebTitle(ctx);
+                // CodeExamples.CSOM101.UpdateTitleOfWeb(ctx, "Davids SharePoint Site");
+                //CodeExamples.CSOM101.ListAllLists(ctx);
+                //CodeExamples.CSOM101.CreateDocumentLibrary(ctx);
+                CodeExamples.CSOM101.CreateGenericList(ctx);
+
             }
 
-                Console.WriteLine("press enter to continue");
+
+
+            Console.WriteLine("press enter to continue");
             Console.ReadLine();
 
         }
