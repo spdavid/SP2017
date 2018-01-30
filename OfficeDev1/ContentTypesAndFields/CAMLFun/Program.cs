d87ef8fdbb9e0059c094176b5bf215f445f500ec
David Opdendries <david@zalo.se>
2018-01-30 11:11:40 +0100

    Products

---
+++ OfficeDev1/ContentTypesAndFields/CAMLFun/Program.cs
@@ -0,0 +1,28 @@
+﻿using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace CAMLFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = Common.Helpers.ContextHelper.GetClientContext("https://folkis2017.sharepoint.com/sites/David/"))
+            {
+                XMLFileSystemTemplateProvider provider = new XMLFileSystemTemplateProvider(@"C:\Users\david\source\repos\SP2017\OfficeDev1\ContentTypesAndFields\CAMLFun", "");
+                string templateName = "Product.xml";
+                ProvisioningTemplate template = provider.GetTemplate(templateName);
+                ctx.Web.ApplyProvisioningTemplate(template);
+
+
+
+            }
+        }
+    }
+}
