70d9625e9c528b0bb1cef3a98882bef90505a2f4
David Opdendries <david@zalo.se>
2018-02-07 11:25:13 +0100

    more mvc and sharepoint

---
+++ OfficeDev1/MyFirstSharePointAddIn/MyFirstSharePointAddInWeb/Controllers/ListFunController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SharePoint.Client;
+using MyFirstSharePointAddInWeb.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstSharePointAddInWeb.Controllers
+{
+    public class ListFunController : Controller
+    {
+        // GET: ListFun
+        public ActionResult Index(Boolean? isSuccess)
+        {
+            if (isSuccess.HasValue && isSuccess.Value)
+            {
+                ViewBag.Message = "your list is created. Good job";
+            }
+
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string NewListTitle, string SPHostUrl)
+        {
+            try
+            {
+
+           
+            using (ClientContext ctx = ContextHelper.GetContext())
+            {
+                ctx.Web.CreateList(ListTemplateType.GenericList, NewListTitle, false);
+            }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+               
+            }
+
+            return RedirectToAction("index",  new { SPHostUrl = SPHostUrl, isSuccess = true });
+
+
+        }
+    }
+}
