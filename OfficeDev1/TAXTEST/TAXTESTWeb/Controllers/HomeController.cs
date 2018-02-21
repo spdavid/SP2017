9b9371c7db8848fdda548c2454810a9220c29e6f
David Opdendries <david@zalo.se>
2018-02-21 15:02:53 +0100

    TAXTEST

---
+++ OfficeDev1/TAXTEST/TAXTESTWeb/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.SharePoint.Client.Taxonomy;
+
+namespace TAXTESTWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            User spUser = null;
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+            using (var ctx = spContext.CreateUserClientContextForSPHost())
+            {
+                if (ctx != null)
+                {
+                  List list=  ctx.Web.GetListByTitle("TaxonomyTest");
+
+                  ListItem item =  list.GetItemById(2);
+                    ctx.Load(item, i => i["TAXTEST"]);
+                    ctx.ExecuteQuery();
+
+                    ViewBag.Items = item["TAXTEST"] as TaxonomyFieldValueCollection;
+
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
