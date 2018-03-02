5b2ba253610abbdf7008902dc24964bb14be3998
David Opdendries <david@zalo.se>
2018-03-02 13:09:20 +0100

    webjobs

---
+++ OfficeDev1/IntroToWebJobs/IntroToWebJobs/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using System.Threading;
+
+namespace IntroToWebJobs
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("hello")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+           // Thread.Sleep(5000);
+            // throw new Exception("something bad happened");
+        }
+
+        public static void FiveSecondTask([TimerTrigger("*/5 *  * * * *")] TimerInfo timer)
+        {
+            Console.WriteLine("This should run every 5 seconds");
+        }
+
+        public static void foo([QueueTrigger("hello2")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+            // Thread.Sleep(5000);
+            // throw new Exception("something bad happened");
+        }
+
+
+
+
+    }
+}
---
+++ OfficeDev1/IntroToWebJobs/IntroToWebJobs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace IntroToWebJobs
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+            // how may tries before poisen queue. default is 5
+            config.Queues.MaxDequeueCount = 2;
+            // minimum seconds to check the queue
+            config.Queues.MaxPollingInterval = TimeSpan.FromSeconds(4);
+
+            // how many items to process at once. Default is 16
+            config.Queues.BatchSize = 2;
+
+            config.UseTimers();
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
